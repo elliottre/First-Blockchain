43ef52fa792a0868803b5465551d835bcab383f7
Elliot Trebajo <trebaell@gmail.com>
2020-01-12 22:24:28 -0500

    first commit

---
+++ blockchain/dev/test.js
@@ -0,0 +1,19 @@
+const Blockchain = require('./blockchain')
+const bitcoin = new Blockchain()
+
+bitcoin.createNewBlock(1234, "woeifjoifjso", "sadf3r23rwefs")
+
+bitcoin.createNewTransaction(100, 'ALEXERWER34RWEFERF', 'JENWR34RERGERTERGD')
+
+bitcoin.createNewBlock(234234, "efwer34rdf", "234234werwef")
+
+bitcoin.createNewTransaction(50, 'ALEXERWER34RWEFERF', 'JENWR34RERGERTERGD')
+bitcoin.createNewTransaction(300, 'ALEXERWER34RWEFERF', 'JENWR34RERGERTERGD')
+bitcoin.createNewTransaction(200, 'ALEXERWER34RWEFERF', 'JENWR34RERGERTERGD')
+
+bitcoin.createNewBlock(2342352, "sdgdge4gf", "sdgergdfgfsdfe3")
+
+
+
+
+console.log(bitcoin)
